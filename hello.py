04c62d910280045ee8d0bbdd2fcb7cc53ed0e0b7
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-04-07 19:42:39 +0530

    pycode1

---
+++ hello.py
@@ -0,0 +1,9 @@
+a= input("Enter two number")
+
+b = input()
+
+
+print("hello how are you" + a + "second value" + b)
+
+sum= int(a)+int(b)
+print("sum is "+str(sum))
---
+++ swapping.py
@@ -0,0 +1,13 @@
+a = input("enter a value")
+b = 25
+
+print(type(a))
+print(type(b))
+
+# swapp
+
+temp = a
+a = b
+b = temp
+
+print("first number " + str(a) + " second number is " + str(b))
