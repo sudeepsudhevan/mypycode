98353c618dcc5a7a7fd362fba82e84bebced9970
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-04-08 17:58:14 +0530

    whilesample

--- if assignme.py
+++ if assignme.py
@@ -1,12 +1,12 @@
 print("1 for Biriyani \n2 for appam\n3 for kadala\n4 for mathi curry ")
 choose=int(input("Enter your choice"))
-if(choose == 1):
+if choose == 1:
     print("you selected Biriyani")
-elif(choose == 2) :
+elif choose == 2:
     print("you selected appam")
-elif(choose == 3):
+elif choose == 3:
     print("you selected kadala")
-elif(choose == 4):
+elif choose == 4:
     print("you selected mathi curry")
 else:
     print("you fool")
