46c66cf40a2487f5ac5d3a9f4f96664c41217921
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-04-08 17:30:17 +0530

    if assignment added

---
+++ if assignme.py
@@ -0,0 +1,12 @@
+print("1 for Biriyani \n2 for appam\n3 for kadala\n4 for mathi curry ")
+choose=int(input("Enter your choice"))
+if(choose == 1):
+    print("you selected Biriyani")
+elif(choose == 2) :
+    print("you selected appam")
+elif(choose == 3):
+    print("you selected kadala")
+elif(choose == 4):
+    print("you selected mathi curry")
+else:
+    print("you fool")
