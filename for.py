b80e427eb8a0951c864f4ba8ee83e88a652e4369
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-04-08 18:58:28 +0530

    pyramid

---
+++ for.py
@@ -0,0 +1,6 @@
+files=["joker.mp4","baba.mp4","jurassic park.mp4"]
+
+for x in files:
+    print(x[0:-4])
+
+print(files)
---
+++ pyramid.py
@@ -0,0 +1,10 @@
+n=int(input("Enter the no.of row"))
+k=n-1
+for i in range(0,n):
+
+    for j in range (0,k):
+        print(end=" ")
+    k=k-1
+    for j in range(0, i+1):
+        print("* ",end='')
+    print(" ")
