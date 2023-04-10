c33a90adee6df7f115a650459cf64ef4d5bf2dd4
Sudeep Sudhevan <sudeepsudhevan6473@gmail.com>
2023-04-10 15:52:50 +0530

    functionsample

---
+++ functionsample.py
@@ -0,0 +1,13 @@
+def hey(name,age):
+    print("My name is "+name + " age : "+str(age))
+
+def hello():
+    print("heello")
+
+value="good"
+hello()
+hey(value,1)
+
+def powe(*values):
+    print("\nfirst: "+values[0]+ " second: "+values[1])
+powe("rehi","sup2")
